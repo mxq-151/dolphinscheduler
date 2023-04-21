d1f13af36536a2a89330e44bdc55a2e40ff48d28
mxq <moxuqiant@ourchem.com>
2023-04-22 00:11:11 +0800

    修复资源相关bug

--- dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/process/ProcessServiceImpl.java
+++ dolphinscheduler-service/src/main/java/org/apache/dolphinscheduler/service/process/ProcessServiceImpl.java
@@ -17,14 +17,6 @@
 
 package org.apache.dolphinscheduler.service.process;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.base.Joiner;
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
-import io.micrometer.core.annotation.Counted;
-
 import static java.util.stream.Collectors.toSet;
 import static org.apache.dolphinscheduler.common.constants.CommandKeyConstants.*;
 import static org.apache.dolphinscheduler.common.constants.Constants.LOCAL_PARAMS;
@@ -32,8 +24,6 @@ import static org.apache.dolphinscheduler.plugin.task.api.enums.DataType.VARCHAR
 import static org.apache.dolphinscheduler.plugin.task.api.enums.Direct.IN;
 import static org.apache.dolphinscheduler.plugin.task.api.utils.DataQualityConstants.TASK_INSTANCE_ID;
 
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.dolphinscheduler.common.constants.CommandKeyConstants;
 import org.apache.dolphinscheduler.common.constants.Constants;
 import org.apache.dolphinscheduler.common.enums.AuthorizationType;
@@ -139,14 +129,10 @@ import org.apache.dolphinscheduler.service.task.TaskPluginManager;
 import org.apache.dolphinscheduler.service.utils.ClusterConfUtils;
 import org.apache.dolphinscheduler.service.utils.DagHelper;
 import org.apache.dolphinscheduler.spi.enums.ResourceType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Nullable;
 import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -162,6 +148,22 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import io.micrometer.core.annotation.Counted;
 
 /**
  * process relative dao that some mappers in this.
@@ -408,7 +410,9 @@ public class ProcessServiceImpl implements ProcessService {
         // add command timezone
         Schedule schedule = scheduleMapper.queryByProcessDefinitionCode(command.getProcessDefinitionCode());
         if (schedule != null) {
-            Map<String, String> commandParams = StringUtils.isNotBlank(command.getCommandParam()) ? JSONUtils.toMap(command.getCommandParam()) : new HashMap<>();
+            Map<String, String> commandParams =
+                    StringUtils.isNotBlank(command.getCommandParam()) ? JSONUtils.toMap(command.getCommandParam())
+                            : new HashMap<>();
             commandParams.put(Constants.SCHEDULE_TIMEZONE, schedule.getTimezoneId());
             command.setCommandParam(JSONUtils.toJsonString(commandParams));
         }
@@ -2468,6 +2472,7 @@ public class ProcessServiceImpl implements ProcessService {
     @Override
     public String getResourceIds(TaskDefinition taskDefinition) {
         Set<Integer> resourceIds = null;
+
         AbstractParameters params = taskPluginManager.getParameters(ParametersNode.builder()
                 .taskType(taskDefinition.getTaskType()).taskParams(taskDefinition.getTaskParams()).build());
 
@@ -2477,10 +2482,20 @@ public class ProcessServiceImpl implements ProcessService {
                     .map(ResourceInfo::getId)
                     .collect(toSet());
         }
+
         if (CollectionUtils.isEmpty(resourceIds)) {
             return "";
         }
-        return Joiner.on(",").join(resourceIds);
+
+        Integer[] resIds = resourceIds.toArray(new Integer[resourceIds.size()]);
+        List<Resource> resourceList = this.resourceMapper.listResourceByIds(resIds);
+
+        if (!resourceList.isEmpty()) {
+            resourceIds =
+                    resourceList.stream().map(Resource::getId).collect(Collectors.toSet());
+            return Joiner.on(",").join(resourceIds);
+        }
+        return "";
     }
 
     @Override
@@ -2494,7 +2509,28 @@ public class ProcessServiceImpl implements ProcessService {
             taskDefinitionLog.setUpdateTime(now);
             taskDefinitionLog.setOperateTime(now);
             taskDefinitionLog.setOperator(operator.getId());
-            taskDefinitionLog.setResourceIds(getResourceIds(taskDefinitionLog));
+
+            String ids = getResourceIds(taskDefinitionLog);
+            if (!StringUtils.isEmpty(ids)) {
+                String[] array = ids.split(",");
+                List<Map<String, Long>> idMap = new ArrayList<>();
+                for (String ele : array) {
+                    Map<String, Long> map = new HashMap<>();
+                    map.put("id", Long.valueOf(ele));
+                    idMap.add(map);
+                }
+                String str = taskDefinitionLog.getTaskParams();
+                ObjectNode node = JSONUtils.parseObject(str);
+                node.set("resourceList", JSONUtils.toJsonNode(idMap));
+                taskDefinitionLog.setTaskParams(JSONUtils.toJsonString(node));
+            } else {
+                String str = taskDefinitionLog.getTaskParams();
+                ObjectNode node = JSONUtils.parseObject(str);
+                node.set("resourceList", JSONUtils.toJsonNode(new ArrayList<>()));
+                taskDefinitionLog.setTaskParams(JSONUtils.toJsonString(node));
+            }
+
+            taskDefinitionLog.setResourceIds(ids);
             if (taskDefinitionLog.getCode() == 0) {
                 taskDefinitionLog.setCode(CodeGenerateUtils.getInstance().genCode());
             }
@@ -2700,7 +2736,30 @@ public class ProcessServiceImpl implements ProcessService {
                 this.findRelationByCode(processDefinition.getCode(), processDefinition.getVersion());
         List<TaskDefinitionLog> taskDefinitionLogList = genTaskDefineList(taskRelations);
         List<TaskDefinition> taskDefinitions =
-                taskDefinitionLogList.stream().map(t -> (TaskDefinition) t).collect(Collectors.toList());
+                taskDefinitionLogList.stream().map(t -> {
+
+                    String ids = getResourceIds(t);
+                    if (!StringUtils.isEmpty(ids)) {
+                        String[] array = ids.split(",");
+                        List<Map<String, Long>> idMap = new ArrayList<>();
+                        for (String ele : array) {
+                            Map<String, Long> map = new HashMap<>();
+                            map.put("id", Long.valueOf(ele));
+                            idMap.add(map);
+                        }
+                        String str = t.getTaskParams();
+                        ObjectNode node = JSONUtils.parseObject(str);
+                        node.set("resourceList", JSONUtils.toJsonNode(idMap));
+                        t.setTaskParams(JSONUtils.toJsonString(node));
+                    } else {
+                        String str = t.getTaskParams();
+                        ObjectNode node = JSONUtils.parseObject(str);
+                        node.set("resourceList", JSONUtils.toJsonNode(new ArrayList<>()));
+                        t.setTaskParams(JSONUtils.toJsonString(node));
+                    }
+                    TaskDefinition task = (TaskDefinition) t;
+                    return task;
+                }).collect(Collectors.toList());
         return new DagData(processDefinition, taskRelations, taskDefinitions);
     }
 
